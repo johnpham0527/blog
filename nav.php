6d58efb14c9859224631a7237f6c50f486df36c3
John Pham <mr.john.pham@gmail.com>
2020-04-08 05:39:17 -0400

    Change nav to link to php pages

--- nav.php
+++ nav.php
@@ -1,3 +1,3 @@
         <h2>Projects</h2>
-        <li><a href="projects/choropleth.html">Choropleth Map</a></li>
-        <li><a href="projects/heatmap.html">Heat Map</a></li>
+        <li><a href="projects/choropleth.php">Choropleth Map</a></li>
+        <li><a href="projects/heatmap.php">Heat Map</a></li>
