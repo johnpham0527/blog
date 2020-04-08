9dfca3244e5fef40debdda5b2b157562799f3be0
John Pham <mr.john.pham@gmail.com>
2020-04-08 06:22:43 -0400

    Delete debug statements

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,3 @@
         <h2>Projects</h2>
         <li><a href="<?php echo $pathToRoot; ?>/projects/choropleth.php">Choropleth Map</a></li>
         <li><a href="<?php echo $pathToRoot; ?>/projects/heatmap.php">Heat Map</a></li>
-        <p><?php echo $pathToRoot; ?></p>
