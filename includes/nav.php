8293cc6ad4286f8d1ac7b33a91d81a2aefab9a24
John Pham <mr.john.pham@gmail.com>
2020-04-08 06:21:24 -0400

    Debugging pathToRoot

--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,4 @@
         <h2>Projects</h2>
         <li><a href="<?php echo $pathToRoot; ?>/projects/choropleth.php">Choropleth Map</a></li>
         <li><a href="<?php echo $pathToRoot; ?>/projects/heatmap.php">Heat Map</a></li>
+        <p><?php echo $pathToRoot; ?></p>
