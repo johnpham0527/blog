7a5dff6458c57b591be39e64478f41696e2410b9
John Pham <mr.john.pham@gmail.com>
2020-04-08 06:26:45 -0400

    Link back to my LinkedIn profile

--- updates/index.php
+++ updates/index.php
@@ -84,7 +84,8 @@
         <h2>About Me</h2>
         <section>
           <p>My name is John D. Pham. I have over 10 years of professional experience in New York City's nonprofit sector, including six in nonprofit development and fundraising. 
-          I am passionate about delivering nonprofit and public services more efficiently, effectively, and equitably.</p>
+          I am passionate about delivering nonprofit and public services more efficiently, effectively, and equitably.
+          Learn more about me on <a href="https://www.linkedin.com/in/johndpham/">LinkedIn</a>.</p>
         </section>
         <h2>About This Site</h2>
         <p>I created this website to blog about my journey in learning web development.
