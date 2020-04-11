fef7eaf31376b0eda2865f88b538af049aace207
John Pham <mr.john.pham@gmail.com>
2020-04-11 13:34:27 -0400

    Update blog index.php to include aside

--- updates/index.php
+++ updates/index.php
@@ -81,17 +81,10 @@
         ?>
       </nav>
       <aside role="complementary">
-        <h2>About Me</h2>
-        <section>
-          <p>My name is John D. Pham. I have over 10 years of professional experience in New York City's nonprofit sector, including six in nonprofit development and fundraising. 
-          I am passionate about delivering nonprofit and public services more efficiently, effectively, and equitably.
-          Learn more about me on <a href="https://www.linkedin.com/in/johndpham/">LinkedIn</a>.</p>
-        </section>
-        <h2>About This Site</h2>
-        <p>I created this website to blog about my journey in learning web development.
-          My blog will describe the challenge and joy of learning JavaScript and other modern web development tools.
-          I will also feature and discuss completed projects on this site.
-          Come join me on my learning journey for using web development for the civic good!</p>
+        <?php 
+        $headerPage = $pathToRoot."/includes/footer.php";
+        include($headerPage); 
+        ?>
       </aside>
       <footer role="contentinfo">
         <?php 
