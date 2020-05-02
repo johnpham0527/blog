8e8b9538fc31e49a7277a562382bcf05b9905356
John Pham <mr.john.pham@gmail.com>
2020-05-02 18:15:39 -0400

    Code is able to read titles from json file

--- updates/read-blogs.js
+++ updates/read-blogs.js
@@ -1,5 +1,17 @@
 "use strict";
 
-fetch('updates.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+fetch("updates.json")
+  .then( response => response.json())
+  .then( data => {
+        console.log(data)
+
+        let output = "";
+        let allPosts = data.posts;
+
+        allPosts.forEach( post => {
+            output += post.title + "<br>";
+        });
+
+        document.getElementById("debug").innerHTML = output;
+
+  });
