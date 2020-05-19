8b38779243cf68508753eef5860a476f357db116
John Pham <mr.john.pham@gmail.com>
2020-05-19 07:16:06 -0400

    Get rid of ul for nav

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,6 +1,4 @@
         <h2>Projects</h2>
-        <ul>
-                <li><a href="<?php echo $pathToRoot; ?>/projects/choropleth.php">Choropleth Map</a></li>
-                <li><a href="<?php echo $pathToRoot; ?>/projects/heatmap.php">Heat Map</a></li>
-                <li><a href="<?php echo $pathToRoot; ?>/projects/pomodoro_clock.php">Pomodoro Clock</a></li>
-        </ul>
+                <a href="<?php echo $pathToRoot; ?>/projects/choropleth.php">Choropleth Map</a>
+                <a href="<?php echo $pathToRoot; ?>/projects/heatmap.php">Heat Map</a>
+                <a href="<?php echo $pathToRoot; ?>/projects/pomodoro_clock.php">Pomodoro Clock</a>
