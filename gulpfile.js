e87002128119fa6b5f1506c3765b280dec146249
John Pham <mr.john.pham@gmail.com>
2020-05-19 06:24:37 -0400

    Create gulpfile for browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const {src, dest, series, parallel, watch} = require('gulp');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const concatenate = require ('gulp-concat');
+const sass = require('gulp-sass');
+
+const origin = 'src';
+const destination = 'build';
+
+sass.compiler = require('node-sass');
+
+async function clean(cb) {
+  await del(destination);
+  cb();
+}
+
+function html(cb) {
+  src(`${origin}/*.html`).pipe(dest(`${destination}`));
+  cb();
+}
+function css(cb) {
+  src(`${origin}/*.css`)
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .pipe(dest(`${destination}/css`));
+  cb();
+}
+
+function js(cb) {
+  src([
+    `${origin}/*.js`
+  ])
+  .pipe(concatenate('build.js'))
+  .pipe(babel())
+  .pipe(dest(`${destination}/js`));
+  cb();
+}
+
+function watcher(cb) {
+  watch(`${origin}/*.html`).on('change', series(html, browserSync.reload));
+  watch(`${origin}/*.css`).on('change', series(css, browserSync.reload));
+  watch(`${origin}/*.js`).on('change', series(js, browserSync.reload));
+  cb();
+}
+
+function server(cb) {
+  browserSync.init({
+    notify: false,
+    open: false,
+    server: {
+      baseDir: destination
+    }
+  });
+  cb();
+}
+
+//exports.default = series(clean, parallel(html, css, js), server, watcher);
+exports.default = series(server, watcher);
